34efed20b8b24a1e5df005a8f496d361ebabee76
doubleZ <doubleZ0108@163.com>
2021-04-21 22:55:52 +0800

    list merge

---
+++ 21-合并两个有序链表.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+# @lc code=end
+
