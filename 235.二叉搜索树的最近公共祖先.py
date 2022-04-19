3383a10d899ad5ce454c165b977d125bcaadb595
doubleZ <doubleZ0108@163.com>
2022-04-19 10:42:21 +0800

    🌲lowest common ancestor(binary-search tree)

---
+++ 235.二叉搜索树的最近公共祖先.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=235 lang=python
+#
+# [235] 二叉搜索树的最近公共祖先
+#
+# 二叉搜索树满足根一定比左子树大，且一定比右子树小
+#
+# 解法1(T93% S38%): 如果当前遍历的节点正好介于两个节点值之间 那它就是最近的公共祖先；如果当前节点比两个数都大，那就进入左子树寻找；否则进入右子树寻找。注意提前把两节点比比大小，把小的值赋给p，大的值赋给q
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if p.val > q.val:
+            p, q = q, p
+
+        while root:
+            if root.val >= p.val and root.val <= q.val:
+                return root
+
+            if root.val > q.val:
+                root = root.left
+            elif root.val < p.val:
+                root = root.right
+        
+# @lc code=end
+
