eecbf631e300764deaf80863dfe194e180bff596
doubleZ <doubleZ0108@163.com>
2022-05-13 15:53:23 +0800

    ✅finish 102 level order

---
+++ 102.二叉树的层序遍历.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=102 lang=python
+#
+# [102] 二叉树的层序遍历
+#
+# 解法1(T81% S40%): 维护一个队列，每次弹出一个值后把它的左右节点放到队列里，不断重复直到队列为空，注意因为是分层输出的，因此每层需要加空值表示一层的结束（题目中因为val的范围是正负1000，因此比较方便设置垃圾值），如果值覆盖很大，可以为每个节点添加另一个元组值表示是第几层的节点
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root: return []
+        
+        queue = [root]
+        res = []
+        while queue:
+            queue.append(TreeNode(9999))
+            tmp = []
+            while True:
+                node = queue.pop(0)
+                if node.val == 9999: break
+                else:
+                    tmp.append(node.val)
+                    if node.left: queue.append(node.left)
+                    if node.right: queue.append(node.right)
+            res.append(tmp)
+        return res
+# @lc code=end
+
